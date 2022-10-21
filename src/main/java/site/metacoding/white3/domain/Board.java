7bde31f693ea4632bf81f95862ea25ec489d75ef
Gammi06 <bpudgw182@naver.com>
2022-10-21 14:26:45 +0900

    board identity 설정 변경함

--- src/main/java/site/metacoding/white3/domain/Board.java
+++ src/main/java/site/metacoding/white3/domain/Board.java
@@ -3,6 +3,7 @@ package site.metacoding.white3.domain;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Getter;
@@ -10,12 +11,12 @@ import lombok.Setter;
 
 @Setter
 @Getter
-@Entity
-public class Board { // 테이블을 자동으로 생성해줌
-    @Id
-    @GeneratedValue
+@Entity // 테이블로 생성
+public class Board {
+    @Id // pk
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto-increamen
     private Long id;
     private String title;
-    @Column(length = 1000)
+    @Column(length = 1000) // 데이터의 크기가 1000자인 열
     private String content;
 }
