bc396b45879f86fd53c09d1d71db9b580de22eb7
Gammi06 <bpudgw182@naver.com>
2022-10-21 12:52:02 +0900

    스프링부트 JPA 세팅 완료

---
+++ src/main/java/site/metacoding/white3/White3Application.java
@@ -0,0 +1,13 @@
+package site.metacoding.white3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class White3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(White3Application.class, args);
+	}
+
+}
---
+++ src/test/java/site/metacoding/white3/White3ApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.white3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class White3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
