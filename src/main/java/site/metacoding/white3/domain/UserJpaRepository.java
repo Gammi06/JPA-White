2b24379d59c3011ade29422b30c352236e5404a2
Gammi06 <bpudgw182@naver.com>
2022-10-27 15:52:37 +0900

    jpa 실습 완료

--- src/main/java/site/metacoding/white3/domain/UserJpaRepository.java
+++ src/main/java/site/metacoding/white3/domain/UserJpaRepository.java
@@ -1,7 +1,13 @@
 package site.metacoding.white3.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserJpaRepository extends JpaRepository<User, Long> {
 
+    @Query(value = "select u from User u where u.username = :username")
+    User findByUsername(@Param("username") String username);
+    // findBy(필드명) 하면 쿼리가 알아서 만들어진다. <완죤 짱이네!!!
+    // jpa 네임드 쿼리 << 하지만 직접 쓰는게 많이 느니까 직접 쓰는게 좋음
 }
